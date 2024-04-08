22682efaf3edb8040e3ea30f57ba4aaea4f57d88
Sreesanjay <sreesanjay7592@gmail.com>
2024-04-09 01:07:10 +0530

    vite config changed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-redux'],
+    },
+  }
 })
